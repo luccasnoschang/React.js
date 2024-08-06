536aa282e10cf3945579e0679397f565b37170a8
Luccas Noschang <Luccas.7noschang@gmail.com>
2024-08-06 20:05:38 -0300

    att

---
+++ meuApp/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+import Conteudo from "../components/Header" 
+
+function Home() {
+  return (
+    <div>
+        <Header />
+        <Conteudo/>
+    </div>
+  )
+}
+
+export default Home
+
+rfce
